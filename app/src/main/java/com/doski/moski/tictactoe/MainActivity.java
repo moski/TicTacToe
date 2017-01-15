57492bd83b1ee4c4a55697445fa6f839def1410d
Mohammed Ersan <mosh_java@yahoo.com>
2017-01-15 19:29:20 +0000

    updating layout structure

--- app/src/main/java/com/doski/moski/tictactoe/MainActivity.java
+++ app/src/main/java/com/doski/moski/tictactoe/MainActivity.java
@@ -2,7 +2,6 @@ package com.doski.moski.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
@@ -26,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		game = new Game(this);
-		hint = (TextView) findViewById(R.id.hint);
+		hint = (TextView) findViewById(R.id.startGameButton);
 		hint.setText(game.hint());
 
 		player1Selector = findViewById(R.id.selectPlayer1);
